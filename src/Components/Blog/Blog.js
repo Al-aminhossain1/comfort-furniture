50f1d6833fa77bf571fe0eebdcc9d7480d7339ad
Al-aminhossain1 <md.al.aminhossain1785@gmail.com>
2022-04-29 18:15:31 +0600

    added a simple navbar

---
+++ src/Components/Blog/Blog.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blog = () => {
+    return (
+        <div>
+            <h4>This is Question Answer part!!</h4>
+        </div>
+    );
+};
+
+export default Blog;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Container, Nav, Navbar, } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className=' container    mx-auto   '>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Comfort-Furniture</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+
+                        <Nav className='mr-auto'>
+                            <Nav.Link as={Link} eventKey={2} to='/'>HOME</Nav.Link>
+                            <Nav.Link as={Link} eventKey={2} to='/blog'>BLOG</Nav.Link>
+                        </Nav>
+                        <Nav >
+                            <Nav.Link as={Link} eventKey={2} to='/login'>LOGIN
+                            </Nav.Link>
+                        </Nav>
+
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+
+
+            {/* <Link to='/'>HOME</Link>
+            <Link to='/blog'>Blogs</Link>
+            <Link to='/login'>LOGIN</Link> */}
+
+        </div>
+    );
+};
+
+export default Header;
